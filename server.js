27dc19644b34fde08d62f86ad050b2caa48d3b4e
Leherguargin <maciejleszczynski1997@gmail.com>
2019-11-06 16:02:51 +0100

    moze to to(tak coś koajrzę)(ale i tak mi nie działao)

--- server.js
+++ server.js
@@ -8,16 +8,9 @@ const inert = require('inert');
 //         cors: true
 //     }
 // });
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+var server = new hapi.Server(+process.env.PORT, '0.0.0.0');
 
 ///////////////////////////////////// route
 //ruting, serwer na rządanie get zwróci to po return
